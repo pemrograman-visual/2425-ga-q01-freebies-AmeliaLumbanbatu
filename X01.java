abf6061af2950d189b1b3664f8382671fa8718db
AmeliaLumbanbatu <renatamelia05@gmail.com>
2024-10-09 16:42:22 +0700

    sudah menyelesaikan tugas

--- X01.java
+++ X01.java
@@ -1,2 +1,36 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24025 - Paula Gevriella Tambunan
+// 12S24031 - Amelia Renata Lumbanbatu
+
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double hargaBuku, totalHarga, hargaBukuTermurah, totalPembayaran;
+
+        hargaBuku = Double.parseDouble(input.nextLine());
+        totalHarga = hargaBuku;
+        hargaBukuTermurah = hargaBuku;
+        while (hargaBuku != 0) {
+            if (hargaBukuTermurah < hargaBuku) {
+                hargaBukuTermurah = hargaBukuTermurah;
+            } else {
+                hargaBukuTermurah = hargaBuku;
+            }
+            hargaBuku = Double.parseDouble(input.nextLine());
+            totalHarga = totalHarga + hargaBuku;
+        }
+        if (totalHarga >= 100) {
+            totalPembayaran = totalHarga - hargaBukuTermurah;
+        } else {
+            totalPembayaran = totalHarga;
+        }
+        System.out.println(toFixed(totalPembayaran,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
